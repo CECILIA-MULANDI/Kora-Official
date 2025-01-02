c2bdf715651415da7bc063a02251241972c7ab83
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-01-03 02:49:20 +0300

    preparations for proof genearation for the IOT DATA

--- zk_integration/sp1_zk/kora/lib/src/lib.rs
+++ zk_integration/sp1_zk/kora/lib/src/lib.rs
@@ -1,20 +1,23 @@
 // use alloy_sol_types::sol;
 use serde::{Deserialize, Serialize};
-use std::{error::Error, io};
+use std::{error::Error, fs::File};
 
-#[derive(Debug, Deserialize)]
-struct IotData {
-    timestamp: u32,
-    latitude: f32,
-    longitude: f32,
-    speed_kmh: f32,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct IotData {
+    pub timestamp: u32,
+    pub latitude: f32,
+    pub longitude: f32,
+    pub speed_kmh: f32,
 }
 
-pub fn read_records() -> Result<(), Box<dyn Error>> {
+pub fn read_records(file_path: &str) -> Result<Vec<IotData>, Box<dyn Error>> {
     // we need to build a reader
-    let mut reader = csv::Reader::from_reader(io::stdin());
+    let file = File::open(&file_path)?;
+    let mut reader = csv::Reader::from_reader(file);
+    let mut records = Vec::new();
     for result in reader.deserialize() {
         let data: IotData = result?;
+        records.push(data);
     }
-    Ok(())
+    Ok(records)
 }
